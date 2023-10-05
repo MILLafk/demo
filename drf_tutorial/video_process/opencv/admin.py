2c1624a1552c2ef0acf6e2f95f94599895cb7c98
MILLafk <73940736+MILLafk@users.noreply.github.com>
2023-10-05 15:00:27 +0800

    Delete drf_tutorial directory

--- drf_tutorial/video_process/opencv/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Video, Image, Frame, Target
-
-# Register your models here.
-admin.site.register(Video)
-admin.site.register(Image)
-admin.site.register(Frame)
-admin.site.register(Target)
--- drf_tutorial/video_process/run.sh
+++
@@ -1,7 +0,0 @@
-rm -r video_process/media/*/
-mkdir video_process/media
-python manage.py flush
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
-python manage.py runserver 127.0.0.1:8000
